620435cba5d62a932b8cff2360d864f8fc4c317c
Adrian Krol <adriankrol11@gmail.com>
2021-09-29 22:52:36 +0200

    add edit posts function

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.HomepageView.as_view(), name='index'),
     path('new-post/', views.get_new_post, name='new_post'),
-    #path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('edit-post/<int:pk>/', views.edit_post, name='edit_post'),
     #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     #path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -23,14 +23,32 @@ def get_new_post(request):
             title = form.cleaned_data['title']
             body = form.cleaned_data['body']
             is_published = form.cleaned_data['is_published']
-
             q = Entry(title=title, body=body,
                       is_published=is_published, pub_date=timezone.now())
-
             q.save()
-
-            print(q)
             return HttpResponseRedirect('/blog/')
     else:
         form = EntryForm()
     return render(request, 'blog/entry_form.html', {'form': form})
+
+
+def edit_post(request, pk=None):
+    q = get_object_or_404(Entry, pk=pk)
+    if request.method == 'POST':
+        form = EntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            body = form.cleaned_data['body']
+            is_published = form.cleaned_data['is_published']
+            q.title = title
+            q.body = body
+            q.is_published = is_published
+            q.pub_date = timezone.now()
+            q.save()
+        return HttpResponseRedirect('/blog/')
+    else:
+        form = EntryForm(initial={
+                     'title': q.title,
+                     'body': q.body,
+                     'is_published': q.is_published})
+    return render(request, 'blog/edit_entry.html', {'form': form, 'id': pk})
